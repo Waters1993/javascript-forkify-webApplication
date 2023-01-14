4a584597fc3e3b4dc5ec6faa30aaa759fc3e06c9
James Waters <jwaters1993@outlook.com>
2023-01-14 10:40:00 -0500

    first commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,68 @@
-const recipeContainer = document.querySelector('.recipe');
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import searchView from './views/searchView.js';
+import recipeView from './views/recipeView.js';
+import paginationView from './views/paginationView.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import resultsView from './views/resultsView.js';
+
+const controlRecipes = async function () {
+  try {
+    // Capture the id from the URL
+    const id = window.location.hash.slice(1);
+
+    // If no id is present return immediately
+    if (!id) return;
+
+    // Display a spinner on load
+    recipeView.renderSpinner();
+
+    // Load recipe will update the recipe in the state object
+    await model.loadRecipe(id);
+    await model.loadSearchResults('pizza');
+
+    // Render recipe view with the current recipe in the state object
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
 };
 
-// https://forkify-api.herokuapp.com/v2
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
 
-///////////////////////////////////////
+    // Load search results
+    await model.loadSearchResults(query);
+
+    // Render results
+    resultsView.render(model.getSearchResultsPage());
+
+    //Render pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  // Render new results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // Render new pagination buttons
+  paginationView.render(model.state.search);
+};
+
+// Publisher subscriber design patter.
+// The view will listen for the events
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+init();
---
+++ src/js/model.js
@@ -0,0 +1,60 @@
+import { API_URL, RES_PER_PAGE } from './config';
+import { getJSON } from './helpers';
+
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+    resultsPerPage: RES_PER_PAGE,
+    page: 1,
+  },
+};
+
+// Function updates the recipe in the state object
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}${id}`);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      ingredients: recipe.ingredients,
+      servings: recipe.servings,
+      cooktime: recipe.cooking_time,
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
+// Function to return search results
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    const { recipes } = data.data;
+
+    state.search.results = recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+  return state.search.results.slice(start, end);
+};
